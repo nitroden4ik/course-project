1a9710bef6a6bc6d00fb811f08029eaea814cd70
Den Petrof <doogieman1992@gmail.com>
2023-05-01 14:29:25 +0300

    add slider

--- script/script.js
+++ script/script.js
@@ -1,4 +1,23 @@
-const swiper = new Swiper('.swiper-wrapper', {
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
   direction: 'horizontal',
+  slidesPerView: 3,
   loop: true,
-})
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
