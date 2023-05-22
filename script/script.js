1e39758ad3a1b76b104ea2b5e3c03d1100665923
Den Petrof <doogieman1992@gmail.com>
2023-05-22 19:59:36 +0300

    working on welcome page

--- script/script.js
+++ script/script.js
@@ -22,3 +22,30 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+
+const swiper2 = new Swiper2('.swiper-container-2', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-next',
+    prevEl: '.swiper-prev',
+
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
